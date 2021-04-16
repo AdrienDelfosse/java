11d8c6abfa478da51908e4227d1b9103ee882d21
Adrien Delfosse <adri.delfosse@hotmail.com>
2021-04-16 13:56:31 +0200

    déclaration des variables d'instances

--- src/be/technifutur/starwars/Scenario1.java
+++ src/be/technifutur/starwars/Scenario1.java
@@ -2,7 +2,11 @@ package be.technifutur.starwars;
 
 public class Scenario1 {
     public static void main(String[] args) {
+        Vaisseau v1 = new Vaisseau("Millénium");
+        Vaisseau v2 = new Vaisseau("Xwing");
+        v1.setNbMissiles(10);
         System.out.println("Il y a très très longtemps dans une galaxie très très lointaine...");
+        System.out.println("Le vaisseau : " + v1.getName() + " a " + v1.getNbMissiles() + " missile(s)");
 
     }
 }
--- src/be/technifutur/starwars/Vaisseau.java
+++ src/be/technifutur/starwars/Vaisseau.java
@@ -5,4 +5,20 @@ public class Vaisseau {
     private String name;
     private int nbMissiles;
 
+    public Vaisseau(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+       return this.name;
+    }
+
+    public int getNbMissiles(){
+        return this.nbMissiles;
+    }
+
+
+    public void setNbMissiles(int nbMissiles) {
+        this.nbMissiles = nbMissiles;
+    }
 }
